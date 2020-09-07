595b600e81e62cf6d76e5fd953e7c6fa470128bd
tslacker <tatsuaki_shiki@slacker.sakura.ne.jp>
2020-09-07 21:24:46 +0900

    test

--- get.js
+++ get.js
@@ -1,18 +1,30 @@
 'use strict';
-function getRandomInt(max) {
-    const object = document.getElementById("object");
-    const fix = Math.floor(Math.random() * Math.floor(max));
-    let comment;
-    if(fix == 1) {
-        comment="大凶";
-    } else if(fix == 2) {
-        comment="凶";
-    } else if(fix == 3) {
-        comment="吉";
-    } else if(fix == 4) {
-        comment="中吉";
-    } else {
-        comment="大吉";
-    }
-    object.textContent = comment + "です。";
-  }
+
+function getRandomInt() {
+    const result = document.getElementById('result');
+    const results = ['大吉', '大凶', '凶', '吉', '中吉'];
+    const fix = Math.floor(Math.random() * results.length);
+    const comment = results[fix];
+
+    result.textContent = comment + 'です。';
+}
+
+// function getRandomInt(max) {
+//     const result = document.getElementById('result');
+//     const fix = Math.floor(Math.random() * max);
+//     let comment;
+
+//     if (fix === 1) {
+//         comment = '大凶';
+//     } else if (fix === 2) {
+//         comment = '凶';
+//     } else if (fix === 3) {
+//         comment = '吉';
+//     } else if (fix === 4) {
+//         comment = '中吉';
+//     } else {
+//         comment = '大吉';
+//     }
+    
+//     result.textContent = comment + 'です。';
+// }
