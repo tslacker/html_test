6cc91fd298e852e265a0ea5f0c0b7b609a215288
tslacker <tatsuaki_shiki@slacker.sakura.ne.jp>
2020-09-13 21:25:36 +0900

    test

--- marubatsu/push.js
+++ marubatsu/push.js
@@ -7,13 +7,16 @@ let player = -1;
 for (let i = 1; i < 10; i++) {
     const newButton = document.createElement("button");
     newButton.id = `Button${i}`;
+    newButton.classList.add('Button');
     newButton.addEventListener("click", () => pushButton(i));
     const getGame = document.getElementById("game");
     getGame.appendChild(newButton);
+
     if (i % 3 === 0) {
         const newBr = document.createElement("br");
         getGame.appendChild(newBr);
     }
+
     buttonCheck[i]=0;
 }
 
@@ -27,16 +30,24 @@ function colorChange(position, player) {
     } else {
         document.getElementById(`Button${position}`).style.background = 'white';
     }
-     return player === 1 ? -1 : 1
+
+    return player === 1 ? -1 : 1;
 }
 
 function judge(fix) {
     if (fix === 3) {
         result.textContent = '青の勝ち';
+        finish();
     } else if (fix === -3) {
         result.textContent = '赤の勝ち';
+        finish();
     }
+}
 
+function finish() {
+    for (let i = 1; i < 10; i++) {
+        document.getElementById(`Button${i}`).disabled = true;
+    }
 }
 
 function pushButton(intNo) {
@@ -49,27 +60,38 @@ function pushButton(intNo) {
     for (let i = 1; i < 10; i += 3) {
         for(let j = i; j < 3 + i; j++) {
             fix = fix + buttonCheck[j];
-            judge(fix)
+            judge(fix);
         }
         fix = 0;
     }
+
     for (let i = 1; i < 4; i++) {
         for(let j = 1 * i; j < 10; j += 3) {
             fix = fix + buttonCheck[j];
-            judge(fix)
+            judge(fix);
         }
         fix = 0;
     }
 
     for (let i = 1; i < 10; i += 4) {
         fix = fix + buttonCheck[i];
-        judge(fix)
+        judge(fix);
     }
     fix = 0;
 
     for (let i = 3; i < 8; i += 2) {
         fix = fix + buttonCheck[i];
-        judge(fix)
+        judge(fix);
     }
     fix = 0;
+
+    let check = 0;
+    for (let i = 1; i < 10; i ++) {
+        if (buttonCheck[i] === 0) {
+            check = check + 1;
+        }
+    }
+    if (check === 0) {
+        result.textContent = '引き分け';
+    }
 }
